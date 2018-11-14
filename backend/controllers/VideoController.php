147909df60a17440a433cc821421e7e3b7ff36ba
askvortsov94 <askvortsov94@gmail.com>
2018-11-14 15:54:22 +0400

    fixAccessControl

--- backend/controllers/VideoController.php
+++ backend/controllers/VideoController.php
@@ -28,6 +28,15 @@ class VideoController extends Controller
                     'delete' => ['POST'],
                 ],
             ],
+            'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
         ];
     }
 
