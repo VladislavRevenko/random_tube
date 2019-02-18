92f445a56f5dc1a93df706fc577ba8287863d670
Vladislav Revenko <vladrevenko@yandex.ru>
2019-02-18 18:12:33 +0400

    task-2950

--- backend/controllers/TemplateController.php
+++ backend/controllers/TemplateController.php
@@ -98,6 +98,7 @@ class TemplateController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
+
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             $dir = $model->attributes['code'];
